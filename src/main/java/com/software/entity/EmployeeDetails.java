24388cd6732e5f7b2432f0994e0ae573b508ed87
soumyakanta <soumyakantabehera59@gmail.com>
2025-03-03 13:19:29 +0530

    TEst

--- src/main/java/com/software/entity/EmployeeDetails.java
+++ src/main/java/com/software/entity/EmployeeDetails.java
@@ -2,24 +2,24 @@ package com.software.entity;
 
 import java.util.List;
 
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table
+@javax.persistence.Entity
+@javax.persistence.Table
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -28,8 +28,6 @@ public class EmployeeDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
-	
 
 	@NotBlank(message = "First Name is mandatory")
 	private String firstName;
@@ -52,5 +50,6 @@ public class EmployeeDetails {
 	@NotNull(message = "Salary is mandatory")
 	@Positive(message = "Salary must be positive")
 	private Double salary;
+	
 
 }
