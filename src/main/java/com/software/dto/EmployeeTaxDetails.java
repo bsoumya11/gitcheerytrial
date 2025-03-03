6960f5ce5401b7e2aad4566b87badc4c76fc2a8a
soumyakanta <soumyakantabehera59@gmail.com>
2025-03-03 11:12:12 +0530

    first commit

--- src/main/java/com/software/dto/EmployeeTaxDetails.java
+++ src/main/java/com/software/dto/EmployeeTaxDetails.java
@@ -5,11 +5,12 @@ import lombok.Data;
 @Data
 public class EmployeeTaxDetails {
 
-	private Integer Id;
+	//private Integer Id;
 	private String firstName;
 	private String lastName;
 	private double yearlySalary;
 	private double taxAmount;
 	private double cessAmount;
+	
 
 }
